17153c29e05c74698f641bef04088be8a71b6cae
Qeesev <1174189882@qq.com>
2021-09-06 22:23:30 +0800

    temp

---
+++ OceanWide/Assets/Scripts/QeeScript/DialogControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogControl : MonoBehaviour
+{
+
+    public Text content;
+    
+
+    public void setContent(string value) {
+        this.content.text = value;
+    }
+
+    public void close() {
+        this.gameObject.SetActive(false);
+    }
+}
