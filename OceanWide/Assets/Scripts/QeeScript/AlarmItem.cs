bb3c09d4e79fdba33b965610989d823f7ae950bc
Qeesev <1174189882@qq.com>
2021-09-26 23:42:17 +0800

    1

---
+++ OceanWide/Assets/Scripts/QeeScript/AlarmItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UIDataStruct;
+
+public class AlarmItem : MonoBehaviour
+{
+    public Text deviceName;
+    public Text monitorName;
+    public Text categoryName;
+    public Text descName;
+
+    private Color color;
+
+    void Start()
+    {
+        this.color = this.transform.GetComponent<RawImage>().color;
+    }
+
+    public void setAlarmItem(AlarmGrid item) {
+        deviceName.text = item.deviceName;
+        monitorName.text = item.monitorName;
+        categoryName.text = item.categoryName;
+        descName.text = item.descName;
+    }
+
+    public void setColor() {
+        this.transform.GetComponent<RawImage>().color = new Color(this.color.r, this.color.g, this.color.b, 0);
+    }
+
+
+
+}
---
+++ OceanWide/Assets/Scripts/QeeScript/DeviceDetailControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceDetailControl : MonoBehaviour
+{
+    public Transform detail_container;
+    private RectTransform detail_prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        detail_prefab = Resources.Load<RectTransform>("UIPrefab/deviceVedio");
+    }
+
+    
+
+    public void close()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void setContainer(DeviceEventType deviceEventType) 
+    {
+        for (int i = 0, child_number = detail_container.childCount; i < child_number; i++)
+        {
+            GameObject.DestroyImmediate(detail_container.GetChild(0).gameObject);
+        }
+
+        if (deviceEventType == DeviceEventType.DeTailInfo)
+        {
+
+        }
+        else if (deviceEventType == DeviceEventType.RecordGrid)
+        {
+
+        }
+        else if (deviceEventType == DeviceEventType.Video)
+        {
+            RectTransform item = GameObject.Instantiate<RectTransform>(detail_prefab, detail_container);
+            UniversalMediaPlayer control = item.GetComponentInChildren<UniversalMediaPlayer>();
+            //control.Path = "";
+            control.Play();
+        }
+    }
+}
