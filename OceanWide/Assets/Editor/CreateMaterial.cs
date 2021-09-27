00ea9bcc8bdead778e7110e6a7b9b218b993d0c4
jmapzhouqing <jmapzhouqing@aliyun.com>
2021-09-27 10:16:31 +0800

    提交

---
+++ OceanWide/Assets/Editor/CreateMaterial.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using System.IO;
+
+public class CreateMaterial : EditorWindow
+{
+    [MenuItem("Assets/CreateMaterial")]
+    public static void create_material() {
+        GameObject[] selected = Selection.gameObjects;
+
+        foreach (GameObject item in selected) {
+            string path = AssetDatabase.GetAssetPath(item);
+            path = Path.GetDirectoryName(path);
+
+            string material_path = Path.Combine(path, "Materials");
+            string texture_path = Path.Combine(path, "texture");
+
+            List<Material> materials = new List<Material>();
+
+            foreach (Material mat in item.GetComponent<MeshRenderer>().sharedMaterials) {
+                string map_path = Path.Combine(texture_path, item.name + ".jpg");
+                string mat_name = item.name + "-" + mat.name+".mat";
+                string mat_path = Path.Combine(material_path, mat_name);
+
+                Material clone_mat = new Material(mat);
+                AssetDatabase.CreateAsset(clone_mat, mat_path);
+
+                Texture texture = AssetDatabase.LoadAssetAtPath<Texture>(map_path);
+
+                clone_mat.SetTexture("_DetailAlbedoMap", texture);
+
+                materials.Add(clone_mat);    
+                
+                //clone_mat.se
+            }
+
+            item.GetComponent<MeshRenderer>().sharedMaterials = materials.ToArray();
+
+
+            Debug.Log(path);
+        }
+    }
+}
--- OceanWide/Assets/Scripts/Control/CameraControl.cs
+++ OceanWide/Assets/Scripts/Control/CameraControl.cs
@@ -31,7 +31,7 @@ public class CameraControl : MonoBehaviour
 
     public Transform target;
 
-    private float speed = 30;
+    private float speed = 60;
     
 
     private Matrix4x4 matrix;
@@ -62,7 +62,7 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update(){
         if (control_state.Equals(MouseControl.LeftDrag)){
-            rotation += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 1;
+            rotation += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 2;
 
             rotation.y = Mathf.Clamp(rotation.y, pitch_min, pitch_max);
         }
@@ -75,7 +75,8 @@ public class CameraControl : MonoBehaviour
         else if (control_state.Equals(MouseControl.WheelDrag)) {
             //this.target.position += new Vector3(10,0,10);
             Vector3 direction = Vector3.Cross(this.transform.right, Vector3.up).normalized;
-            target_position += -1 * this.transform.right * Input.GetAxis("Mouse X") - direction * Input.GetAxis("Mouse Y");
+            target_position += (-1 * this.transform.right * Input.GetAxis("Mouse X") - direction * Input.GetAxis("Mouse Y"))*4
+                ;
         }
     }
 
