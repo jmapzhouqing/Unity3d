8d012d85008b71a49f8ec58dc752922ec439211d
jmapzhouqing <jmapzhouqing@aliyun.com>
2021-10-13 21:34:46 +0800

    修改

--- OceanWide/Assets/Editor/Util.cs
+++ OceanWide/Assets/Editor/Util.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using LitJson;
 
 public class Util:EditorWindow
 {
     [MenuItem("tools/prfab")]
     public static void ReplacePrefab() {
         GameObject[] selected = Selection.gameObjects;
-        GameObject origin = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/球机.prefab", typeof(GameObject)) as GameObject;
+        GameObject origin = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/电表.prefab", typeof(GameObject)) as GameObject;
         foreach (GameObject item in selected) {
             
             GameObject child = PrefabUtility.InstantiatePrefab(origin) as GameObject;
@@ -26,6 +27,30 @@ public class Util:EditorWindow
             Vector3 euler = item.transform.eulerAngles;
             child.transform.eulerAngles = new Vector3(euler.x + 90, euler.y, euler.z);*/
             GameObject.DestroyImmediate(item);
+        }
+    }
+
+    [MenuItem("tools/CreateInfo")]
+    public static void CreateInfo()
+    {
+        GameObject[] selected = Selection.gameObjects;
+        foreach (GameObject item in selected)
+        {
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+            
+            for (int i = 0, len = item.transform.childCount; i < len; i++) {
+                Transform child = item.transform.GetChild(i);
+                List<string> ids = new List<string>();
+
+                for (int j = 0, child_len = child.childCount; j < child_len; j++) {
+                    Transform _child = child.GetChild(j); 
+                    ids.Add(_child.name);
+                }
+
+                dic.Add(child.name, ids);
+            }
+
+            Debug.Log(JsonMapper.ToJson(dic));
         }
     }
 }
