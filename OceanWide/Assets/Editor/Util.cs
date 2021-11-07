50e1f5d92ddaecfc00da8cf46eebfcdc4c32bc75
jmapzhouqing <jmapzhouqing@aliyun.com>
2021-11-07 21:40:25 +0800

    修改

--- OceanWide/Assets/Editor/Util.cs
+++ OceanWide/Assets/Editor/Util.cs
@@ -9,23 +9,39 @@ public class Util:EditorWindow
     [MenuItem("tools/prfab")]
     public static void ReplacePrefab() {
         GameObject[] selected = Selection.gameObjects;
-        GameObject origin = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/枪机.prefab", typeof(GameObject)) as GameObject;
+        GameObject origin_q = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/device/枪机.prefab", typeof(GameObject)) as GameObject;
+        GameObject origin_Q = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/device/球机.prefab", typeof(GameObject)) as GameObject;
+
+        GameObject origin = AssetDatabase.LoadAssetAtPath("Assets/Resource/prefab/device/水表.prefab", typeof(GameObject)) as GameObject;
         foreach (GameObject item in selected) {
+            GameObject child = null;
             
-            GameObject child = PrefabUtility.InstantiatePrefab(origin) as GameObject;
+            /*
+            if (item.transform.childCount == 2)
+            {
+                child = PrefabUtility.InstantiatePrefab(origin_q) as GameObject;
+            }
+            else {
+                child = PrefabUtility.InstantiatePrefab(origin_Q) as GameObject;
+            }*/
+
+            child = PrefabUtility.InstantiatePrefab(origin) as GameObject;
+
+            child.transform.localScale = new Vector3(1, 1, 1);
 
             child.transform.SetParent(item.transform.parent);
             child.name = item.name;
             child.transform.position = item.transform.position;
-            child.transform.rotation = item.transform.rotation;
+            
+            //child.transform.rotation = item.transform.rotation;
 
             /*Vector3 euler = item.transform.rotation.eulerAngles;
 
             child.transform.rotation = Quaternion.Euler(euler.x,euler.y,euler.z+90);*/
 
-            
+            /*
             Vector3 euler = item.transform.eulerAngles;
-            child.transform.eulerAngles = new Vector3(euler.x, euler.y, euler.z);
+            child.transform.eulerAngles = new Vector3(euler.x, euler.y, euler.z);*/
             GameObject.DestroyImmediate(item);
         }
     }
